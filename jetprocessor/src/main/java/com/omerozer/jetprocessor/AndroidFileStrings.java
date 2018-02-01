60b8cfc078c17627ca905d5f282ad61add0d6195
OmerUygurOzer <omer@travelbank.com>
2018-02-01 15:59:08 -0800

    android file strings updated

--- jetprocessor/src/main/java/com/omerozer/jetprocessor/AndroidFileStrings.java
+++ jetprocessor/src/main/java/com/omerozer/jetprocessor/AndroidFileStrings.java
@@ -6,7 +6,7 @@ package com.omerozer.jetprocessor;
 public class AndroidFileStrings {
 
     static final String ANDROID_THREAD_SWITCHER_SIMPLE = "AndroidThreadSwitcher";
-    static final String ANDROID_THREAD_SWITCHER_QUALIFIED = "com.omerozer.jetcore.AndroidThreadSwitcher";
+    static final String ANDROID_THREAD_SWITCHER_QUALIFIED = "com.omerozer.jet.AndroidThreadSwitcher";
     static final String HANDLER_CLASS = "android.os.Handler";
     static final String LOOPER = "android.os.Looper";
     static final String GET_MAIN_LOOPER = "getMainLooper()";
